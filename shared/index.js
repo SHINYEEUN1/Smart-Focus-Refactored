c65652c7bbc9bf9610431dfed52c005854dc8d2f
kachoret77-prog <kachoret77@gmail.com>
2026-04-02 10:20:20 +0900

    shared 파일 수정및 추가

--- shared/index.js
+++ shared/index.js
@@ -1,6 +1,7 @@
 const { POSTURE_STATUS, POSE_TYPE, GOOD_POSTURE_STATUS } = require('./constants/posture');
 const { SOCKET_EVENTS } = require('./constants/socket-events');
 const { RESPONSE_CODES } = require('./constants/response-codes');
+const { AUTH_PROVIDERS } = require('./constants/auth-providers');
 const { DEFAULT_ANALYSIS_RESULT, createAnalysisResult } = require('./schemas/analysis');
 const { createSuccessResponse, createFailResponse } = require('./utils/response-shape');
 
@@ -10,6 +11,7 @@ module.exports = {
   GOOD_POSTURE_STATUS,
   SOCKET_EVENTS,
   RESPONSE_CODES,
+  AUTH_PROVIDERS,
   DEFAULT_ANALYSIS_RESULT,
   createAnalysisResult,
   createSuccessResponse,
